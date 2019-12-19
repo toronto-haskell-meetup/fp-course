464ff7ed41ae588b7e9fdc32bd29a2fa0f1e2cc8
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-12-19 18:55:26 -0500

    stuck on <**>w

--- src/Course/Monad.hs
+++ src/Course/Monad.hs
@@ -36,8 +36,8 @@ instance Monad ExactlyOne where
     (a -> ExactlyOne b)
     -> ExactlyOne a
     -> ExactlyOne b
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance ExactlyOne"
+  (=<<) f (ExactlyOne a) = f a
+
 
 -- | Binds a function on a List.
 --
@@ -48,8 +48,8 @@ instance Monad List where
     (a -> List b)
     -> List a
     -> List b
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance List"
+  (=<<) _ Nil = Nil
+  (=<<) f (x :. xs) = f x ++ (f =<< xs)
 
 -- | Binds a function on an Optional.
 --
@@ -60,8 +60,8 @@ instance Monad Optional where
     (a -> Optional b)
     -> Optional a
     -> Optional b
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance Optional"
+  (=<<) _ Empty = Empty
+  (=<<)  f (Full a) = f a
 
 -- | Binds a function on the reader ((->) t).
 --
@@ -69,11 +69,8 @@ instance Monad Optional where
 -- 119
 instance Monad ((->) t) where
   (=<<) ::
-    (a -> ((->) t b))
-    -> ((->) t a)
-    -> ((->) t b)
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance ((->) t)"
+    (a -> t -> b) -> (t -> a) -> (t -> b)
+  (=<<) f g = \t -> f (g t) t
 
 -- | Witness that all things with (=<<) and (<$>) also have (<*>).
 --
@@ -111,8 +108,9 @@ instance Monad ((->) t) where
   f (a -> b)
   -> f a
   -> f b
-(<**>) =
-  error "todo: Course.Monad#(<**>)"
+(<**>) f m =  f
+
+
 
 infixl 4 <**>
 
